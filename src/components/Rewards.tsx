a0c7bd2ef96adc5c57fc743eb54beec7f9e78f6c
izmerlik <izmerlik@gmail.com>
2025-05-29 23:58:45 +0700

    fix: precise import order and spacing for ESLint in Rewards.tsx

--- src/components/Rewards.tsx
+++ src/components/Rewards.tsx
@@ -3,7 +3,6 @@
 import { Box, Button, Input, Heading, Stack, Icon } from '@chakra-ui/react';
 import { DragDropContext, Droppable, Draggable, DropResult } from '@hello-pangea/dnd';
 import { collection, query, where, getDocs, addDoc, updateDoc, deleteDoc, doc } from 'firebase/firestore';
-
 import React, { useState, useEffect, useCallback } from 'react';
 import { FiPlus } from 'react-icons/fi';
 
